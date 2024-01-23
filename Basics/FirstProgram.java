240b8ab208df265e1192228472d710138f213999
Shrey K <srkkadia@gmail.com>
2024-01-23 20:59:14 +0530

    FirstProgram Basics

---
+++ Basics/FirstProgram.java
@@ -0,0 +1,44 @@
+public class FirstProgram {
+    public static void main(String args[]) {
+        // System.out.println("Hello World!");
+        // Primitive DT
+        int i = 23;
+        char c = 'w';
+        double d = 232456;
+        float f = 22132.22f;
+        String s = "Jay Shree Ram";   //Non-Primitive DT
+
+        System.out.println("int\t= " + i);
+        System.out.println("char\t= " + c);
+        System.out.println("double\t= " + d);
+        System.out.println("float\t= " + f);
+        System.out.println("String\t= " + s);
+
+        // for (i = 0; i < args.length; i++) {
+        //     System.out.print(args[i] + ", ");
+        // }
+        // System.out.println("\b\b-");
+
+        // Array - size -> sizeof(arr)/sizeof(arr[0])
+    }
+}
+/*
+//println function will print the data as well as append a new line at the end
+System Class -> out (object of PrintStream) -> println("string data");
+
+//print function will print the data
+System Class -> out (object of PrintStream) -> print("string data");
+
+memory -> 1. normal memory -> 2. static memory
+
+cpp
+obj.class_variable
+class_name::class_variable
+
+java
+obj.data_member
+class_name.data_member
+
+
+
+*/
