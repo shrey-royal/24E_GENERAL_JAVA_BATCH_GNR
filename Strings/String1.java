6125618c5c1bf0be0ba4d89de01aa19e045fff3b
Shrey K <srkkadia@gmail.com>
2024-02-13 21:07:19 +0530

    String Intro + Theory

---
+++ Strings/String1.java
@@ -0,0 +1,48 @@
+//String is class
+
+public class String1 {
+    public static void main(String[] args) {
+        String str = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum nemo alias consequuntur dolorum odit aperiam in sed, autem culpa accusantium voluptates, facilis cupiditate quia natus. Possimus, maxime quod cum esse natus aliquam. Ab placeat reiciendis, veritatis blanditiis itaque eos quibusdam nam ipsa nemo dicta temporibus velit minima voluptates dignissimos beatae.";
+
+        // String str1 = new String();
+        // String str2 = new String("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum nemo alias consequuntur dolorum odit aperiam in sed, autem culpa accusantium voluptates, facilis cupiditate quia natus. Possimus, maxime quod cum esse natus aliquam. Ab placeat reiciendis, veritatis blanditiis itaque eos quibusdam nam ipsa nemo dicta temporibus velit minima voluptates dignissimos beatae.");
+
+        //Methods
+
+        // System.out.println(str.charAt(4));
+        // System.out.println(str.length());
+        // System.out.println(str.substring(10));
+        // System.out.println(str.substring(10, 15));
+        // System.out.println(str.indexOf(' ', 7));
+        // System.out.println(str.indexOf(", ", 7));
+        // System.out.println(str.lastIndexOf("a", 360));
+        // System.out.println(str.substring(339, 345));
+        //startswith()
+
+
+    }
+}
+
+/*
+String Methods:
+1. charAt(int index): Returns the character at the specified index.
+2. length(): Returns the length of the string.
+3. substring(int beginIndex): Returns a substring starting from the specified index.
+4. substring(int beginIndex, int endIndex): Returns a substring within the specified range.
+5. indexOf(int ch): Returns the index of the first occurrence of a specified character.
+6. indexOf(int ch, int fromIndex): Returns the index of the first occurrence of a specified character starting from the given index.
+7. indexOf(String str): Returns the index of the first occurrence of a specified substring.
+8. lastIndexOf(int ch): Returns the index of the last occurrence of a specified character.
+9. lastIndexOf(int ch, int fromIndex): Returns the index of the last occurrence of a specified character before the given index.
+10. lastIndexOf(String str): Returns the index of the last occurrence of a specified substring.
+11. startsWith(String prefix): Checks if the string starts with the specified prefix.
+12. endsWith(String suffix): Checks if the string ends with the specified suffix.
+13. toLowerCase(): Converts the string to lowercase.
+14. toUpperCase(): Converts the string to uppercase.
+15. trim(): Removes leading and trailing white spaces.
+16. replace(char oldChar, char newChar): Replaces all occurrences of a character with another character.
+17. replace(CharSequence target, CharSequence replacement): Replaces all occurrences of a specified CharSequence.
+18. contains(CharSequence s): Checks if the string contains the specified sequence of characters.
+19. isEmpty(): Checks if the string is empty.
+20. valueOf(Object obj): Returns the string representation of an object.
+*/
